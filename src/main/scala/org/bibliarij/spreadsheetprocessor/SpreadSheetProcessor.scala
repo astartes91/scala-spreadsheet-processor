ce67f8ae3638c8e3337763047bdc3bcb75df10fa
Vladimir Nizamutdinov <v.nizamutdinov@softage.ru>
2018-05-29 19:06:43 +0700

    Expression processing

--- src/main/scala/org/bibliarij/spreadsheetprocessor/SpreadSheetProcessor.scala
+++ src/main/scala/org/bibliarij/spreadsheetprocessor/SpreadSheetProcessor.scala
@@ -9,35 +9,39 @@ class SpreadSheetProcessor(inputSpreadSheet: Seq[Seq[String]]) {
   private val cellReferenceRegex: String = "[A-Za-z][0-9]"
   private val operatorsRegex: String = """\+|-|\*|/"""
 
-  private val outputSeq: Seq[Seq[String]] = Seq.empty
+  private val outputSeq: mutable.Seq[mutable.Seq[String]] =
+    mutable.Seq.fill(inputSpreadSheet.length, inputSpreadSheet(0).length) {""}
 
   /**
     * @should return correct result
     * @return
     */
   def process(): SpreadSheet = {
-    val seq: Seq[Seq[String]] = inputSpreadSheet
-      .map(
-        inputRow => {
-          inputRow.map(
-            inputCell => {
-              if(inputCell.startsWith("'")){
-                inputCell.replaceFirst("'", "")
-              } else if(inputCell.startsWith("=")) {
-                processExpression(inputCell)
-              } else {
-                try{
-                  inputCell.toLong
-                  inputCell
-                } catch {
-                  case nfe: NumberFormatException => "#Error"
-                }
-              }
-            }
-          )
-        }
-      )
-    new SpreadSheet(seq)
+
+    for (i <- inputSpreadSheet.indices){
+      for (j <- inputSpreadSheet(i).indices){
+        outputSeq(i)(j) = processCell(inputSpreadSheet(i)(j))
+      }
+    }
+
+    new SpreadSheet(outputSeq)
+  }
+
+  private def processCell(inputCell: String): String = {
+    if(inputCell.isEmpty){
+      inputCell
+    } else if(inputCell.startsWith("'")){
+      inputCell.replaceFirst("'", "")
+    } else if(inputCell.startsWith("=")) {
+      processExpression(inputCell)
+    } else {
+      try{
+        inputCell.toLong
+        inputCell
+      } catch {
+        case nfe: NumberFormatException => "#Error"
+      }
+    }
   }
 
   private def processExpression(inputCell: String): String = {
@@ -45,7 +49,20 @@ class SpreadSheetProcessor(inputSpreadSheet: Seq[Seq[String]]) {
     val operatorsArray: Array[Char] = expression.toCharArray.filter(_.toString.matches(operatorsRegex))
     val operators: mutable.Queue[Char] = mutable.Queue(operatorsArray: _*)
     val operands: mutable.Queue[String] = mutable.Queue(expression.split(operatorsRegex): _*)
+
     var operand: String = operands.dequeue()
+    if(operands.isEmpty && operators.isEmpty){
+      if (operand.matches(cellReferenceRegex)){
+        return getCellReferenceValue(operand)
+      } else {
+        try {
+          operand.toLong
+          return operand
+        } catch {
+          case nfe: NumberFormatException => return "#Error"
+        }
+      }
+    }
 
     try {
       while(operands.nonEmpty){
@@ -75,7 +92,12 @@ class SpreadSheetProcessor(inputSpreadSheet: Seq[Seq[String]]) {
   }
 
   private def getCellReferenceValue(cellReference: String): String = {
-    "#Error"
+    val i: Int = Integer.valueOf(cellReference(1).toString) - 1
+    val j: Int = cellReference(0).toUpper - 65
+    if (outputSeq(i)(j).isEmpty){
+      outputSeq(i)(j) = processCell(inputSpreadSheet(i)(j))
+    }
+    outputSeq(i)(j)
   }
 }
 
--- src/test/scala/org/bibliarij/spreadsheetprocessor/SpreadSheetProcessorTest.scala
+++ src/test/scala/org/bibliarij/spreadsheetprocessor/SpreadSheetProcessorTest.scala
@@ -10,13 +10,23 @@ class SpreadSheetProcessorTest {
       */
     @Test
     def process_shouldReturnCorrectResult(): Unit = {
-      val inputSeq: Seq[Seq[String]] = Seq(Seq("1", "'Test", "Error", "=1+2"))
+      val inputSeq: Seq[Seq[String]] = Seq(Seq("1", "'Test"), Seq("Error", "=1+2"), Seq("", ""))
       val outputSpreadSheet: SpreadSheet = new SpreadSheetProcessor(inputSeq).process()
       val outputSeq: Seq[Seq[String]] = outputSpreadSheet.getInternalSeq
-      val row: Seq[String] = outputSeq(0)
-      Assertions.assertThat(row(0)).isEqualTo("1")
-      Assertions.assertThat(row(1)).isEqualTo("Test")
-      Assertions.assertThat(row(2)).isEqualTo("#Error")
-      Assertions.assertThat(row(3)).isEqualTo("3")
+      val row1: Seq[String] = outputSeq(0)
+      val row2: Seq[String] = outputSeq(1)
+      val row3: Seq[String] = outputSeq(2)
+      Assertions.assertThat(row1(0)).isEqualTo("1")
+      Assertions.assertThat(row1(1)).isEqualTo("Test")
+      Assertions.assertThat(row2(0)).isEqualTo("#Error")
+      Assertions.assertThat(row2(1)).isEqualTo("3")
+      Assertions.assertThat(row3(0)).isEqualTo("")
     }
+
+  @Test
+  def processs(): Unit = {
+    val inputSeq: Seq[Seq[String]] = Seq(Seq("1", "2", "=C2"), Seq("1", "2", "3"))
+    val outputSpreadSheet: SpreadSheet = new SpreadSheetProcessor(inputSeq).process()
+    val outputSeq: Seq[Seq[String]] = outputSpreadSheet.getInternalSeq
+  }
 }
