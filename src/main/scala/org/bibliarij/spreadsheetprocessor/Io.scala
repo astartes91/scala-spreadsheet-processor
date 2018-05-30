c4303b2a899a80e4318d19f7b5eb9b8d935c489f
Vladimir Nizamutdinov <v.nizamutdinov@softage.ru>
2018-05-30 14:04:12 +0700

    minor fixes

--- src/main/scala/org/bibliarij/spreadsheetprocessor/Io.scala
+++ src/main/scala/org/bibliarij/spreadsheetprocessor/Io.scala
@@ -6,8 +6,12 @@ class Io (inputReader: InputReader) {
     val spreadsheetParametersStr: String = inputReader.readLine()
     val spreadSheetParameters: Array[String] = spreadsheetParametersStr.split("\t")
     require(spreadSheetParameters.length == 2)
+
     val spreadSheetHeight: Long = spreadSheetParameters(0).toLong
+    require(spreadSheetHeight > 0)
+
     val spreadSheetWeight: Long = spreadSheetParameters(1).toLong
+    require(spreadSheetWeight > 0)
 
     val spreadSheet: Seq[Seq[String]] =
       for (i <- 1L to spreadSheetHeight) yield {
